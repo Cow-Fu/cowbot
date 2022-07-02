774a111e2839604561a32961eb4190cadf859e81
nathan <nathanj758@gmail.com>
2022-07-01 20:05:19 -0400

    add space so bot doesn't tell me to fuck myself

--- SpeechSanitizer.py
+++ SpeechSanitizer.py
@@ -9,35 +9,38 @@ from nextcord.ext import commands
 
 
 class SpeechSanitizer:
-    
-    async def _sanitize_user(self, text: str, ctx: commands.Context):
-        user_list = []
 
-        for match in self.regex_user.finditer(text):
+    async def _sanitize_user_or_channel(self, text: str, ctx: commands.Context, is_user: bool):
+        obj_list = []
+        regex = None
+        prefix = None
+        func = None
+        if is_user:
+            regex = self.regex_user
+            prefix = "@"
+            func = ctx.guild.fetch_member
+        else:
+            regex = self.regex_channel
+            prefix = "#"
+            func = ctx.guild.fetch_channel
+            
+        for match in regex.finditer(text):
             full_match = match.group(0)
             id = int(match.group(1))
-            print(id)
 
-            user = await ctx.guild.fetch_member(id)
-            user_list.append((full_match, f"@{user.display_name}"))
+            obj = await func(id)
+            obj_list.append((full_match, f"{prefix} {obj.display_name if is_user else obj.name}"))
             
-        for full_match, name in user_list:
+        for full_match, name in obj_list:
             text = text.replace(full_match, name)
         return text    
-        
+     
+    
+    async def _sanitize_user(self, text: str, ctx: commands.Context):
+        return await self._sanitize_user_or_channel(text, ctx, True)
+              
     async def _sanitize_channel(self, text: str, ctx: commands.Context):
-        channel_list = []       
-
-        for match in self.regex_channel.finditer(text):
-            full_match = match.group(0)
-            id = int(match.group(1))
-
-            channel = await ctx.guild.fetch_channel(id)
-            channel_list.append((full_match, f"#{channel.name}"))
-
-        for full_match, name in channel_list:
-            text = text.replace(full_match, name)
-        return text
+        return await self._sanitize_user_or_channel(text, ctx, False)
 
     async def sanitize(self, text, ctx: commands.Context):
         text = self.regex_url.sub("link", text) 

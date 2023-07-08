5260511a31fa9a282188eafa743f578304fb3eff
nathan <nathanj758@gmail.com>
2023-07-08 01:34:54 +0000

    added commands to handle cogs from inside discord

--- cowbot.py
+++ cowbot.py
@@ -1,6 +1,8 @@
 import os
-from nextcord.ext import commands
+from nextcord.ext import commands, application_checks
 import nextcord
+from nextcord.ext.commands import errors as nc_errors
+from CogManager import CogManager
 
 from dotenv import load_dotenv
 from voicebot import TTSBot
@@ -25,6 +27,62 @@ async def guess(ctx):
 @guess.command()
 async def what(ctx: commands.Context):
     await ctx.channel.send("Chicken butt")
+
+
+@bot.slash_command()
+async def cog(interaction: nextcord.Interaction):
+    pass
+
+
+@cog.subcommand()
+@commands.check_any(commands.is_owner())
+async def load(interaction: nextcord.Interaction, cog_name: str):
+    responce = None
+    try:
+        bot.load_extension(cog_name)
+    except nc_errors.ExtensionNotFound:
+        responce = f"Extention: '{cog_name}' not found."
+    except nc_errors.ExtensionAlreadyLoaded:
+        responce = f"Extention: '{cog_name}' already loaded."
+    except nc_errors.NoEntryPointError:
+        responce = f"Extention: '{cog_name}' does not have a setup function."
+    except nc_errors.ExtensionFailed:
+        responce = f"Extention: '{cog_name}' the extention or setup function had an execution error."
+    finally:
+        await interaction.send(responce if responce else f"Successfully loaded: '{cog_name}'", ephemeral=True)
+
+
+@cog.subcommand()
+@commands.check_any(commands.is_owner())
+async def unload(interaction: nextcord.Interaction, cog_name: str):
+    responce = None
+    try:
+        bot.unload_extension(cog_name)
+    except nc_errors.ExtensionNotFound:
+        responce = f"Extention: '{cog_name}' not found."
+    except nc_errors.ExtensionNotLoaded:
+        responce = f"Extention: '{cog_name}' is not currently loaded."
+    finally:
+        await interaction.send(responce if responce else f"Successfully unloaded: '{cog_name}'", ephemeral=True)
+
+
+@cog.subcommand()
+@commands.check_any(commands.is_owner())
+async def reload(interaction: nextcord.Interaction, cog_name: str):
+    responce = None
+    try:
+        bot.reload_extension(cog_name)
+    except nc_errors.ExtensionNotLoaded:
+        responce = f"Extention: '{cog_name}' is not currently loaded."
+    except nc_errors.ExtensionNotFound:
+        responce = f"Extention: '{cog_name}' not found."
+    except nc_errors.NoEntryPointError:
+        responce = f"Extention: '{cog_name}' does not have a setup function."
+    except nc_errors.ExtensionFailed:
+        responce = f"Extention: '{cog_name}' the extention or setup function had an execution error."
+    finally:
+        await interaction.send(responce if responce else f"Successfully unloaded: '{cog_name}'", ephemeral=True)
+
 
 extentions = None
 for root, dirs, files in os.walk("cogs"):

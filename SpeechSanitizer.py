c74a837e8d1765b3d8410efecbbcbcf1c0446b8a
nathan <nathanj758@gmail.com>
2024-06-03 23:09:12 -0400

    use raw strings to prevent escaping

--- SpeechSanitizer.py
+++ SpeechSanitizer.py
@@ -18,36 +18,36 @@ class SpeechSanitizer:
             regex = self.regex_channel
             prefix = "#"
             func = ctx.guild.fetch_channel
-            
+
         for match in regex.finditer(text):
             full_match = match.group(0)
             id = int(match.group(1))
 
             obj = await func(id)
             obj_list.append((full_match, f"{prefix} {obj.display_name if is_user else obj.name}"))
-            
+
         for full_match, name in obj_list:
             text = text.replace(full_match, name)
-        return text    
-     
-    
+        return text
+
     async def _sanitize_user(self, text: str, ctx: commands.Context):
         return await self._sanitize_user_or_channel(text, ctx, True)
-              
+
     async def _sanitize_channel(self, text: str, ctx: commands.Context):
         return await self._sanitize_user_or_channel(text, ctx, False)
 
+    # TODO change to take the message itself, then can check for attachments and convert them to text
     async def sanitize(self, text, ctx: commands.Context):
-        text = self.regex_url.sub("link", text) 
+        text = self.regex_url.sub("link", text)
         text = self.regex_emoji.sub(lambda m: m.group(1), string=text)
         text = await self._sanitize_user(text, ctx)
         text = await self._sanitize_channel(text, ctx)
-        
+
         return text
-    
+
     def __init__(self, client: commands.Bot):
-        self.regex_emoji = re.compile("<:(\w+):(\d+)>")
-        self.regex_user = re.compile("<@(\d+)>")
-        self.regex_channel = re.compile("<#(\d+)>")
-        self.regex_url = re.compile("http\S+")
+        self.regex_emoji = re.compile(r"<:(\w+):(\d+)>")
+        self.regex_user = re.compile(r"<@(\d+)>")
+        self.regex_channel = re.compile(r"<#(\d+)>")
+        self.regex_url = re.compile(r"http\S+")
         self.client = client
